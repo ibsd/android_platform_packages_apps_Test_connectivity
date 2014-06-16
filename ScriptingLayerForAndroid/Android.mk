a393c8fe86972c9cc9dc66b4c1e73f2dbd1a5960
Ang Li <angli@google.com>
2014-06-16 13:56:11 -0700

    Disable proguard check. Make sl4a to be installed as priviliged app
    Merging kwd commit f5bc3fbc9957fe5c2d6a4b3ab130355962ef2c12
    Change-Id: I7265973f1b0341068252f25caf450a147d215da2

--- ScriptingLayerForAndroid/Android.mk
+++ ScriptingLayerForAndroid/Android.mk
@@ -1,4 +1,3 @@
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -13,25 +12,16 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libGoogleAnalytics:../Common/libs/libGoogleAnalytics.jar
-
-
-LOCAL_STATIC_JAVA_LIBRARIES := guava android-common libGoogleAnalytics locale_platform
-#LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+LOCAL_STATIC_JAVA_LIBRARIES := guava android-common locale_platform
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.Utils sl4a.Common sl4a.BluetoothFacade
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.SignalStrengthFacade sl4a.TextToSpeechFacade sl4a.WebCamFacade
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.InterpreterForAndroid sl4a.ScriptingLayer sl4a.QuickAction
 
-LOCAL_MODULE_TAGS := tests
-
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_MODULE_TAGS := eng
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
-
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := locale_platform:libs/locale_platform.jar
-include $(BUILD_MULTI_PREBUILT)
-
 
 # only include rules to build other stuff for the original package, not the derived package.
 #ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
