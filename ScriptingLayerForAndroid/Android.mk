eca06f11862f7225346e7d2a758178e308e89475
Ang Li <angli@google.com>
2014-06-17 10:39:41 -0700

    Revert "Revert "Disable proguard check. Make sl4a to be installed as priviliged app""
    This reverts commit 3b33a1b31137709295e4fe417c7927d7107f9a5f.
    
    Conflicts:
    	Tools/sl4a/ScriptingLayerForAndroid/Android.mk
    
    Change-Id: Ic11b5169395542936a73f37171c92488f1afc093

--- ScriptingLayerForAndroid/Android.mk
+++ ScriptingLayerForAndroid/Android.mk
@@ -1,4 +1,3 @@
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -20,16 +19,11 @@ LOCAL_STATIC_JAVA_LIBRARIES += sl4a.Utils sl4a.Common sl4a.BluetoothFacade
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.SignalStrengthFacade sl4a.TextToSpeechFacade sl4a.WebCamFacade
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.InterpreterForAndroid sl4a.ScriptingLayer sl4a.QuickAction
 
-LOCAL_MODULE_TAGS := tests
-
-LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_MODULE_TAGS := eng
+LOCAL_PRIVILEGED_MODULE := true
+LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
-
-include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := locale_platform:libs/locale_platform.jar
-include $(BUILD_MULTI_PREBUILT)
-
 
 # only include rules to build other stuff for the original package, not the derived package.
 #ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
