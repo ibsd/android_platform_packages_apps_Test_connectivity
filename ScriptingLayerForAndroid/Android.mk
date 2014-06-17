624bba12e1344a02e779aca71b70fd7ede58d238
Ang Li <angli@google.com>
2014-06-17 10:26:20 -0700

    Fix make file to fix build Fix a bug in android_device
    Merging kwd commit 5861bf1edda18872676d02749794ed64da67dfc8
    Change-Id: Id063ea653f47c7743102c912a8480c0410df6465

--- ScriptingLayerForAndroid/Android.mk
+++ ScriptingLayerForAndroid/Android.mk
@@ -2,7 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-
 LOCAL_PACKAGE_NAME := sl4a
 
 LOCAL_CERTIFICATE := platform
@@ -12,9 +11,8 @@ LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libGoogleAnalytics:../Common/libs/libGoogleAnalytics.jar
-
-LOCAL_STATIC_JAVA_LIBRARIES := guava android-common locale_platform GoogleAnalytics
+LOCAL_JAVA_LIBRARIES := libGoogleAnalytics
+LOCAL_STATIC_JAVA_LIBRARIES := guava android-common locale_platform
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.Utils sl4a.Common sl4a.BluetoothFacade
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.SignalStrengthFacade sl4a.TextToSpeechFacade sl4a.WebCamFacade
 LOCAL_STATIC_JAVA_LIBRARIES += sl4a.InterpreterForAndroid sl4a.ScriptingLayer sl4a.QuickAction
@@ -25,10 +23,13 @@ LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
 
+include $(CLEAR_VARS)
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := locale_platform:libs/locale_platform.jar
+include $(BUILD_MULTI_PREBUILT)
+
+
 # only include rules to build other stuff for the original package, not the derived package.
 #ifeq ($(strip $(LOCAL_PACKAGE_OVERRIDES)),)
 # additionally, build unit tests in a separate .apk
 include $(call all-makefiles-under,$(LOCAL_PATH))
 #endif
-
-
