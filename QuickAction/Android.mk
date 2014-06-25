763b6b7f85bd204cb2e30098ba4a53e204aa7b9e
Ang Li <angli@google.com>
2014-06-25 16:32:52 -0700

    Fix sl4a build.
    Merging kwd commit 0f4222dadb2485b9d1a3df3308aa0e06280143f8
    Change-Id: I76e5768ac0e6f2ab8b94ef310f7187e822d85b22

--- QuickAction/Android.mk
+++ QuickAction/Android.mk
@@ -7,6 +7,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := sl4a.QuickAction
 LOCAL_STATIC_JAVA_LIBRARIES := guava android-common
 LOCAL_SRC_FILES := $(call all-java-files-under, src/net/londatiga/android)
-
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
 
 include $(BUILD_STATIC_JAVA_LIBRARY)
