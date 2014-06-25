93132aa8c9be4c58f5c5acc0665bb7bffa83bfef
Keith Dart <dart@google.com>
2014-06-25 16:26:01 -0700

    Fixed the copyright owner.
    Required to be Android Open Source Project.
    Everyone change your templates, if you have one.
    
    Change-Id: I470743e59d64dd840053ca0dd69a0ee1c4283e92

--- test_android.py
+++ test_android.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3.4
 # vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
 
-#   Copyright 2014- Google, Inc.
+#   Copyright 2014- The Android Open Source Project
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
