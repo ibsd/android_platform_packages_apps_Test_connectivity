1d393f15922ef92a6fc970ab879720f9fa91a125
Erik Pasternak <roboerik@android.com>
2014-07-16 00:32:18 +0000

    Revert "Update MediaSession constructor"
    The SDK change this depends on hasn't been submitted yet.
    
    This reverts commit f842a57129c87aa1a6e626435815052e15a17fde.
    
    Change-Id: Iebf532a0a278b1db3f0d796028d0379c12037ce2

--- Common/src/com/googlecode/android_scripting/facade/MediaSessionFacade.java
+++ Common/src/com/googlecode/android_scripting/facade/MediaSessionFacade.java
@@ -21,6 +21,7 @@ public class MediaSessionFacade extends RpcReceiver {
 
     private final Service mService;
     private final EventFacade mEventFacade;
+    private MediaSessionManager mMediaManager;
     private MediaSession mMedia;
     private Callback mCallback;
 
@@ -29,7 +30,8 @@ public class MediaSessionFacade extends RpcReceiver {
         mService = manager.getService();
         mEventFacade = manager.getReceiver(EventFacade.class);
         Log.d("Creating MediaSession.");
-        mMedia = new MediaSession(mService, "SL4A");
+        mMediaManager = (MediaSessionManager) mService.getSystemService(Context.MEDIA_SESSION_SERVICE);
+        mMedia = mMediaManager.createSession("SL4A");
         mMedia.setFlags(MediaSession.FLAG_HANDLES_MEDIA_BUTTONS);
         mMedia.setActive(true);
         mCallback = new ButtonCallback(mEventFacade);
