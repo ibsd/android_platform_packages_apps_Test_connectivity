727e7e45678c4c9e43961eeb85ac772048b350fa
Nathan Harold <nharold@google.com>
2015-01-30 16:53:28 -0800

    SL4A WiFi Calling API Additions to ImsManager
    Change-Id: Ib7ff172b860bf4f989090f08dc53d20b894909cb

--- Common/src/com/googlecode/android_scripting/facade/tele/ImsManagerFacade.java
+++ Common/src/com/googlecode/android_scripting/facade/tele/ImsManagerFacade.java
@@ -23,6 +23,7 @@ import android.telephony.SubscriptionManager;
 
 import com.android.ims.ImsException;
 import com.android.ims.ImsManager;
+import com.android.ims.ImsConfig;
 import com.googlecode.android_scripting.Log;
 import com.googlecode.android_scripting.facade.FacadeManager;
 import com.googlecode.android_scripting.jsonrpc.RpcReceiver;
@@ -48,12 +49,12 @@ public class ImsManagerFacade extends RpcReceiver {
 
     @Rpc(description = "Return True if Enhanced 4g Lte mode is enabled by platform.")
     public boolean imsIsEnhanced4gLteModeSettingEnabledByPlatform() {
-        return ImsManager.isVolteEnabledByPlatform(mService);
+        return ImsManager.isVolteEnabledByPlatform(mContext);
     }
 
     @Rpc(description = "Return True if Enhanced 4g Lte mode is enabled by user.")
     public boolean imsIsEnhanced4gLteModeSettingEnabledByUser() {
-        return ImsManager.isEnhanced4gLteModeSettingEnabledByUser(mService);
+        return ImsManager.isEnhanced4gLteModeSettingEnabledByUser(mContext);
     }
 
     @Rpc(description = "Set Enhanced 4G mode.")
@@ -61,6 +62,100 @@ public class ImsManagerFacade extends RpcReceiver {
             throws ImsException{
         ImsManager.setEnhanced4gLteModeSetting(mService, enable);
     }
+
+    /**************************
+     * Begin WFC Calling APIs
+     **************************/
+
+    @Rpc(description = "Return True if WiFi Calling is enabled for platform.")
+    public boolean imsIsWfcEnabledByPlatform() {
+        return ImsManager.isWfcEnabledByPlatform(mContext);
+    }
+
+    @Rpc(description = "Set whether or not WFC is enabled during roaming")
+    public void imsSetWfcRoamingSetting(
+                        @RpcParameter(name = "enable")
+            Boolean enable) {
+        ImsManager.setWfcRoamingSetting(mContext, enable);
+
+    }
+
+    @Rpc(description = "Return True if WiFi Calling is enabled during roaming.")
+    public boolean imsIsWfcRoamingEnabledByUser() {
+        return ImsManager.isWfcRoamingEnabledByUser(mContext);
+    }
+
+    @Rpc(description = "Set the Wifi Calling Mode of operation")
+    public void imsSetWfcMode(
+                        @RpcParameter(name = "mode")
+            String mode)
+            throws IllegalArgumentException {
+
+        int mode_val;
+
+        switch (mode.toUpperCase()) {
+            case "WIFI_ONLY":
+                mode_val =
+                        ImsConfig.WfcModeFeatureValueConstants.WIFI_ONLY;
+                break;
+            case "CELLULAR_PREFERRED":
+                mode_val =
+                        ImsConfig.WfcModeFeatureValueConstants.CELLULAR_PREFERRED;
+                break;
+            case "WIFI_PREFERRED":
+                mode_val =
+                        ImsConfig.WfcModeFeatureValueConstants.WIFI_PREFERRED;
+                break;
+            case "DISABLED":
+                if (ImsManager.isWfcEnabledByPlatform(mContext) &&
+                        ImsManager.isWfcEnabledByUser(mContext) == true) {
+                    ImsManager.setWfcSetting(mContext, false);
+                }
+                return;
+            default:
+                throw new IllegalArgumentException("Invalid WfcMode");
+        }
+
+        ImsManager.setWfcMode(mContext, mode_val);
+        if (ImsManager.isWfcEnabledByPlatform(mContext) &&
+                ImsManager.isWfcEnabledByUser(mContext) == false) {
+            ImsManager.setWfcSetting(mContext, true);
+        }
+
+        return;
+    }
+
+    @Rpc(description = "Return current WFC Mode if Enabled.")
+    public String imsGetWfcMode() {
+        if(ImsManager.isWfcEnabledByUser(mContext) == false) {
+            return "DISABLED";
+        }
+       switch(ImsManager.getWfcMode(mContext)) {
+           case ImsConfig.WfcModeFeatureValueConstants.WIFI_PREFERRED:
+               return "WIFI_PREFERRED";
+           case ImsConfig.WfcModeFeatureValueConstants.CELLULAR_PREFERRED:
+               return "CELLULAR_PREFERRED";
+           case ImsConfig.WfcModeFeatureValueConstants.WIFI_ONLY:
+               return "WIFI_ONLY";
+           default:
+               return "UNKNOWN";
+       }
+    }
+
+    @Rpc(description = "Return True if WiFi Calling is enabled by user.")
+    public boolean imsIsWfcEnabledByUser() {
+        return ImsManager.isWfcEnabledByUser(mContext);
+    }
+
+    @Rpc(description = "Set whether or not WFC is enabled")
+    public void imsSetWfcSetting(
+                        @RpcParameter(name = "enable")  Boolean enable) {
+        ImsManager.setWfcSetting(mContext,enable);
+    }
+
+    /**************************
+     * End WFC Calling APIs
+     **************************/
 
     @Override
     public void shutdown() {
