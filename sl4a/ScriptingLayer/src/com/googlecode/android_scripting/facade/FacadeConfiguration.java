319e0e4b32246e0b47dde6ab56544a96cabfcd95
Etan Cohen <etancohen@google.com>
2015-12-15 14:21:36 -0800

    NAN: Facade for WifiNanManager
    Initial facade for WifiNanManager - exposing the discovery
    methods to be used for sl4a testing.
    
    Bug: 26216681
    Change-Id: Ifc8fe035de146a0fc84601dad1d64f11483f40d2

--- sl4a/ScriptingLayer/src/com/googlecode/android_scripting/facade/FacadeConfiguration.java
+++ sl4a/ScriptingLayer/src/com/googlecode/android_scripting/facade/FacadeConfiguration.java
@@ -56,6 +56,7 @@ import com.googlecode.android_scripting.facade.telephony.TelephonyManagerFacade;
 import com.googlecode.android_scripting.facade.ui.UiFacade;
 import com.googlecode.android_scripting.facade.wifi.HttpFacade;
 import com.googlecode.android_scripting.facade.wifi.WifiManagerFacade;
+import com.googlecode.android_scripting.facade.wifi.WifiNanManagerFacade;
 import com.googlecode.android_scripting.facade.wifi.WifiP2pManagerFacade;
 import com.googlecode.android_scripting.facade.wifi.WifiRttManagerFacade;
 import com.googlecode.android_scripting.facade.wifi.WifiScannerFacade;
@@ -133,8 +134,9 @@ public class FacadeConfiguration {
         sFacadeClassList.add(CarrierConfigFacade.class);
 
         /*Compatibility reset to >= Marshmallow */
-        if( sSdkLevel > 23 ) {
+        if( sSdkLevel >= 23 ) {
             //add new facades here
+            sFacadeClassList.add(WifiNanManagerFacade.class);
         }
 
         for (Class<? extends RpcReceiver> recieverClass : sFacadeClassList) {
