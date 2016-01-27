04615c13a4bdb47055fc9840ae988059ecdf2c6d
Etan Cohen <etancohen@google.com>
2016-01-27 09:18:32 -0800

    NAN: Fix spelling error in name of onConfigCompleted event.
    Also fixed style checker issues.
    
    Change-Id: Ie0caf59646fc409a5ee325f2ba02988aa477d708

--- sl4a/Common/src/com/googlecode/android_scripting/facade/wifi/WifiNanManagerFacade.java
+++ sl4a/Common/src/com/googlecode/android_scripting/facade/wifi/WifiNanManagerFacade.java
@@ -1,17 +1,17 @@
 /*
- * Copyright (C) 2016 Google Inc.
+ * Copyright (C) 2016 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.googlecode.android_scripting.facade.wifi;
@@ -20,15 +20,15 @@ import android.app.Service;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.wifi.nan.ConfigRequest;
-import android.net.wifi.nan.WifiNanEventListener;
-import android.net.wifi.nan.WifiNanSession;
-import android.net.wifi.nan.WifiNanSessionListener;
 import android.net.wifi.nan.PublishData;
 import android.net.wifi.nan.PublishSettings;
 import android.net.wifi.nan.SubscribeData;
 import android.net.wifi.nan.SubscribeSettings;
 import android.net.wifi.nan.TlvBufferUtils;
+import android.net.wifi.nan.WifiNanEventListener;
 import android.net.wifi.nan.WifiNanManager;
+import android.net.wifi.nan.WifiNanSession;
+import android.net.wifi.nan.WifiNanSessionListener;
 import android.os.Bundle;
 import android.os.HandlerThread;
 import android.os.Looper;
@@ -289,7 +289,7 @@ public class WifiNanManagerFacade extends RpcReceiver {
         public void onConfigCompleted(ConfigRequest configRequest) {
             Bundle mResults = new Bundle();
             mResults.putParcelable("config", configRequest);
-            mEventFacade.postEvent("WifiNanOnConfigFailed", mResults);
+            mEventFacade.postEvent("WifiNanOnConfigCompleted", mResults);
         }
 
         @Override
