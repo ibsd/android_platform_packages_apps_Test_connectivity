b2d4eb942c465b06d84145d2166f9ece18c9598d
Christopher Wiley <wiley@google.com>
2016-06-17 07:18:13 -0700

    Fix SL4N build
    Bug: 29396611
    Change-Id: Ie3a8132a7353e9faed35eadba4e57de107b13400
    Test: Builds

--- sl4n/Android.mk
+++ sl4n/Android.mk
@@ -22,8 +22,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := sl4n
 
 LOCAL_C_INCLUDES += \
-  frameworks/opt/net/wifi/service/jni \
-  hardware/libhardware_legacy/include/hardware_legacy \
   system/bt \
   $(LOCAL_PATH)/rapidjson/include \
   $(LOCAL_PATH)/facades
@@ -41,7 +39,7 @@ LOCAL_SHARED_LIBRARIES += \
   libcutils \
   libutils \
   libhardware \
-  libhardware_legacy \
+  libwifi-hal \
   liblog
 
 LOCAL_STATIC_LIBRARIES += \
@@ -49,25 +47,7 @@ LOCAL_STATIC_LIBRARIES += \
   libosi \
   libbluetooth-client
 
-# set correct Wi-Fi HAL library path and add Wi-Fi related libraries
-# ============================================================
-LIB_WIFI_HAL := libwifi-hal
-
-ifeq ($(BOARD_WLAN_DEVICE), bcmdhd)
-  LIB_WIFI_HAL := libwifi-hal-bcm
-else ifeq ($(BOARD_WLAN_DEVICE), qcwcn)
-  LIB_WIFI_HAL := libwifi-hal-qcom
-else ifeq ($(BOARD_WLAN_DEVICE), mrvl)
-  # this is commented because none of the nexus devices
-  # that sport Marvell's wifi have support for HAL
-  # LIB_WIFI_HAL := libwifi-hal-mrvl
-else ifeq ($(BOARD_WLAN_DEVICE), MediaTek)
-  # support MTK WIFI HAL
-  LIB_WIFI_HAL := libwifi-hal-mt66xx
-endif
-
 LOCAL_STATIC_LIBRARIES += \
-  $(LIB_WIFI_HAL) \
   libnl \
   libwifi-hal-stub
 
